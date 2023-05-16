da9c2217176b28b764ba868a93139def86a91469
maicon-vieira-dx <maicon10042001@gmail.com>
2023-05-16 18:32:33 -0300

    Início

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { InputComponent } from './components/input/input.component';
+import { ItemComponent } from './components/item/item.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InputComponent,
+    ItemComponent
+  ],
+  imports: [
+    BrowserModule,
+    FontAwesomeModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/item/item.component.ts
@@ -0,0 +1,36 @@
+import { Component, Input, OnChanges, OnInit, Output, EventEmitter, OnDestroy } from '@angular/core';
+import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { Item } from 'src/app/interfaces/iItem';
+
+@Component({
+  selector: 'app-item',
+  templateUrl: './item.component.html',
+  styleUrls: ['./item.component.css']
+})
+export class ItemComponent implements OnInit, OnChanges, OnDestroy {
+  @Input() item!: Item;
+  @Output() emitindoItemParaEditar = new EventEmitter;
+  @Output() emitindoIdParaDeletar = new EventEmitter;
+  faPen = faPen;
+  faTrash = faTrash
+
+  constructor() { }
+
+  ngOnInit(): void { }
+
+  ngOnChanges() {
+
+  }
+
+  editarItem(){
+    this.emitindoItemParaEditar.emit(this.item)
+  }
+
+  deletarItem(){
+    this.emitindoIdParaDeletar.emit(this.item)
+  }
+
+  ngOnDestroy(){
+    console.log('Destruiu')
+  }
+}
